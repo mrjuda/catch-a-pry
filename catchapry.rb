d4b776c97f9ac2c2ef7fd24e1cef76b757e78cd7
Bushido-brown <orizut@gmail.com>
2022-09-03 10:27:37 -0700

    fix error

--- catchapry.rb
+++ catchapry.rb
@@ -1,46 +1,148 @@
 # catchapry.rb
 
 @morse_code_dictionary = {
+
+
+
   '.-' => 'A',
+
+
+
   '-...' => 'B',
+
+
+
   '-.-.' => 'C',
+
+
+
   '-..' => 'D',
+
+
+
   '.' => 'E',
+
+
+
   '..-.' => 'F',
+
+
+
   '--.' => 'G',
+
+
+
   '....' => 'H',
+
+
+
   '..' => 'I',
+
+
+
   '.---' => 'J',
+
+
+
   '-.-' => 'K',
+
+
+
   '.-..' => 'L',
+
+
+
   '--' => 'M',
+
+
+
   '-.' => 'N',
+
+
+
   '---' => 'O',
+
+
+
   '.--.' => 'P',
+
+
+
   '--.-' => 'Q',
+
+
+
   '.-.' => 'R',
+
+
+
   '...' => 'S',
+
+
+
   '-' => 'T',
+
+
+
   '..-' => 'U',
+
+
+
   '...-' => 'V',
+
+
+
   '.--' => 'W',
+
+
+
   '-..-' => 'X',
+
+
+
   '-.--' => 'Y',
+
+
+
   '--..' => 'Z'
+
+
+
 }
 
-def decode_char(letter)
-  @morse_code_dictionary[letter]
+
+
+def decode_char(letters)
+
+  @morse_code_dictionary[letters]
+
 end
 
-def decode_word(word)
-  letters = word.split.map { |letter| decoed_char(letter) }
+
+
+def decode_word(words)
+
+  letters = words.split.map { |letters| decode_char(letters) }
+
+
+
   letters.join
+
 end
+
+
 
 def decode(code)
-  wodrs = code.split('   ').map { |word| decode_word(word) }
+
+  words = code.split('   ').map { |word| decode_word(word) }
+
+
+
   words.join('  ')
+
 end
 
+
+
 puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+
