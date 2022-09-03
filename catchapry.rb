e2f4fb7bc0431944378b73a1310927819272ab28
judah <87juda@gmail.com>
2022-09-03 13:40:45 -0300

    Create catchapry.rb file

--- catchapry.rb
+++ catchapry.rb
@@ -1,1 +1,46 @@
 # catchapry.rb
+
+@morse_code_dictionary = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(letter)
+  @morse_code_dictionary[letter]
+end
+
+def decode_word(word)
+  letters = word.split.map { |letter| deceod_char(letter) }
+  letters.join
+end
+
+def decode(code)
+  words = code.split('   ').map { |word| decode_word(word) }
+  words.join('  ')
+end
+
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
